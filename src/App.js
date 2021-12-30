805db2b577909e375d856af5cb50ff53448ae9b9
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-12-30 12:06:24 +0700

    change the api key

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [favourites, setFavourites] = useState([]);
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=d109dc46`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
